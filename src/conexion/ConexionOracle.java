ab7690f69544c120956243b9066c47d949a46c62
gtapiero <giovanny.tapiero@pi.edu.co>
2026-05-23 11:57:05 -0500

    commit - EmpleadoDAO .

--- src/conexion/ConexionOracle.java
+++ src/conexion/ConexionOracle.java
@@ -37,4 +37,4 @@ public class ConexionOracle {
 
         return con;
     }
-}   
+}
--- src/dao/EmpleadoDAO.java
+++ src/dao/EmpleadoDAO.java
@@ -1,4 +1,13 @@
 package dao;
 
+import conexion.ConexionOracle;
+import java.sql.*;
+
 public class EmpleadoDAO {
+
+    Connection con;
+
+    public EmpleadoDAO() {
+        con = ConexionOracle.conectar();
+    }   
 }
